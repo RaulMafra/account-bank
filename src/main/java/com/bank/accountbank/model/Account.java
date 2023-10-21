0a79d20e8bec05ab8e8852516d552fd63f75f173
RaulMafra <raulcesar.sm@gmail.com>
2023-10-21 16:36:19 -0300

    feat: created entity

---
+++ src/main/java/com/bank/accountbank/model/Account.java
@@ -0,0 +1,65 @@
+package com.bank.accountbank.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
+	private String number;
+	private String agency;
+	@Column(scale = 13, precision = 2)
+	private BigDecimal balance;
+	@Column(name = "overdraft", scale = 13, precision = 2)
+	private BigDecimal limite;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getAgency() {
+		return agency;
+	}
+
+	public void setAgency(String agency) {
+		this.agency = agency;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public BigDecimal getLimite() {
+		return limite;
+	}
+
+	public void setLimite(BigDecimal limite) {
+		this.limite = limite;
+	}
+
+}
---
+++ src/main/java/com/bank/accountbank/model/Feature.java
@@ -0,0 +1,8 @@
+package com.bank.accountbank.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
