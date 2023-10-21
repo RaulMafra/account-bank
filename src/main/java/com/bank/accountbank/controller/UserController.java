03440642b2d398a8e2de7d73aadbfaafa5f9e418
RaulMafra <raulcesar.sm@gmail.com>
2023-10-21 20:47:40 -0300

    feat: created UserController layer

---
+++ src/main/java/com/bank/accountbank/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.bank.accountbank.controller;
+
+import com.bank.accountbank.model.User;
+import com.bank.accountbank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
