ddef3c4c17f3cf98e6389d71514695721e04d0ce
RaulMafra <raulcesar.sm@gmail.com>
2023-10-21 20:52:01 -0300

    feat: method for search the account number crated

--- src/main/java/com/bank/accountbank/repository/UserRepository.java
+++ src/main/java/com/bank/accountbank/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.bank.accountbank.repository;
 
+import com.bank.accountbank.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bank.accountbank.model.User;
+public interface UserRepository extends JpaRepository<User, Long> {
 
-public interface  UserRepository extends JpaRepository<User, Long>{
+    boolean existsByAccountNumber(String accountNumber);
 
 }
